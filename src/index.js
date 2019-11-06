d471d947ae765a50da6591a1b772daf41e525ea3
usman556 <usmanrehmat@renesistech.com>
2019-11-06 10:55:16 +0500

    worked with Relational data

--- src/index.js
+++ src/index.js
@@ -2,29 +2,56 @@ import { GraphQLServer } from 'graphql-yoga';
 
 // Scalar Types: String, Boolean, Int, Float, ID
 
+const users = [
+    { id: '1', name: 'Jamie', age: null, postId: '1' },
+    { id: '2', name: 'Andrew', age: 27, postId: '2' },
+    { id: '3', name: 'Katie', age: null, postId: '1' }
+]
+
+const posts = [
+    { id: '1', title: 'hjdsbf', body: 'hjdsbfjk', published: true, author: '1', commentId: '1' },
+    { id: '2', title: 'sdnjf', body: 'sdbfjkb', published: false, author: '2', commentId: '2' },
+    { id: '3', title: 'sdjkf', body: 'sdhjodsh', published: true, author: '1', commentId: '3' },
+]
+
+const comments = [
+    { id: '1', text: 'Nice laptop', author: '1', postId: '1' },
+    { id: '2', text: 'Nice keyboard', author: '2', postId: '2' },
+    { id: '3', text: 'Nice Jacket', author: '1', postId: '1' },
+    { id: '4', text: 'Nice Shoes', author: '2', postId: '2' }
+]
 
 //Type definations (schema)
 const typeDefs = `
  type Query {
-     greeting(name: String, position: String): String! 
-     add(numbers: [Float!]!): Float!
-     grades: [Int!]!
-     me: User!
-     post: Post!
+     users(query: String): [User!]!
+     posts(query: String): [Post!]!
+     comments: [Comment!]!
  }
  
  type User {  
      id: ID!
-     name: String!
+     name: String!  
      email: String!
      age: Int
+     posts: [Post!]!
+     comments: [Comment!]!
  }
-
+                                                                                                                                             
  type Post {
-     id: ID!
+     id: ID! 
      title: String!
      body: String!
      published: Boolean!
+     author: User!
+     comments: [Comment!]!
+ }
+
+ type Comment {
+     id: ID!
+     text: String!
+     author: User!
+     post: Post!
  }
 
 `
@@ -32,38 +59,51 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
     Query: {
-        greeting(parent, args, ctx, info) {
-            if (args.name && args.position) {
-                return `Hello ${args.name} with position ${args.position}`
+        users(parent, args, ctx, info) {
+            if (!args.query) {
+                return users;
             }
+            return users.filter((user) => user.name.toLowerCase().includes(args.query.toLowerCase()))
         },
-        add(parent, args, ctx, info) {
-            if (args.numbers.length === 0) {
-                return 0;
+        posts(parent, args, ctx, info) {
+            if (!args.query) {
+                return posts;
             }
-            return args.numbers.reduce((accomulator, currentValue) => {
-                return accomulator + currentValue
+            return posts.filter(post =>
+                (post.title.toLowerCase().includes(args.query.toLowerCase()) || post.body.toLowerCase().includes(args.query.toLowerCase())))
+        },
+        comments(parent, args, ctx, info) {
+            return comments;
+        }
+    },
+    Post: {
+        author(parent, args, ctx, info) {
+            return users.find(user => {
+                return user.id === parent.author
             })
         },
-        grades(parent, args, ctx, info) {
-            return [99, 45, 45]
-        },
-        me() {
-            return {
-                id: '1234',
-                name: 'Usman',
-                email: 'usman@gmail.com'
-            }
-        },
-        post() {
-            return {
-                id: '123',
-                title: 'story telling',
-                body: 'body content',
-                published: false
-            }
+        comments(parent, args, ctx, info) {
+            return comments.filter(comment => comment.postId === parent.id)
         }
-    }
+    },
+    User: {
+        posts(parent, args, ctx, info) {
+            // console.log(parent)
+            return posts.filter(post => post.id === parent.postId)
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter(comment => comment.author === parent.id)
+        }
+    },
+    Comment: {
+        author(parent, args, ctx, info) {
+            return users.find(user => user.id === parent.author)
+        },
+        post(parent, args, ctx, info) {
+            return posts.find(post => post.id === parent.postId)
+        }
+    },
+
 }
 
 const server = new GraphQLServer({
